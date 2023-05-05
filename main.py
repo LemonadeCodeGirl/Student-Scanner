bd9b7b9286af26bf97d369d94da7cf39dc676c0b
LemonadeCodeGirl <42980263+LemonadeCodeGirl@users.noreply.github.com>
2023-05-05 13:51:36 -0400

    File Structure

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-msg = "Hello Wordeld"
+import eel
+eel.init('templates')
 
-print(msg)
+@eel.expose
+def demo(x):
+    return x**2
+
+# 1000 is width of window and 600 is the height
+eel.start('index.html', size=(1000, 600))
