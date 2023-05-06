2104ac7571923fe3b2e8e5df0c3b433840c4179b
LemonadeCodeGirl <42980263+LemonadeCodeGirl@users.noreply.github.com>
2023-05-05 22:11:46 -0400

    Code Updates

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+#Imports
 import eel
+import mysql.connector
+from random import randint
+
+from datetime import datetime
+from datetime import date
+
+
 eel.init('templates')
 
 @eel.expose
-def demo(x):
-    return x**2
-
 def random_python():
     print("Random function running")
-    return randint(1,100)
+    x = randint(1,100)
+    print(x)
+    return x
+
+random_python()
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="yourusername",
+#   password="yourpassword"
+# )
+
+# mycursor = mydb.cursor()
 
 # 1000 is width of window and 600 is the height
 eel.start('index.html', size=(1000, 600))
+
+#######main code
+
--- templates/main.js
+++
@@ -1,8 +0,0 @@
-// Onclick of the button
-document.getElementById("testButton1").onclick = function () {  
-  // Call python's random_python function
-  eel.random_python()(function(number){                      
-    // Update the div with a random number returned by python
-    document.getElementById("number").innerHTML = number;
-  })
-}
